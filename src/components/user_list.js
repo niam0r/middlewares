6f7a8006a6ac8b699e4b908f4c479ccd864b11ff
Romain niam0r <niamor@mm.st>
2018-04-04 15:35:10 +0100

    working and completed up to lecture 51

--- src/components/user_list.js
+++ src/components/user_list.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-class UserList extends React.Component {
+class UserList extends Component {
+  componentWillMount() {
+    this.props.fetchUsers();
+  }
+
   renderUser(user) {
     return (
       <div className="card card-block">
@@ -20,3 +25,10 @@ class UserList extends React.Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  return { users: state.users };
+}
+
+export default connect(mapStateToProps, actions)(UserList);
+
