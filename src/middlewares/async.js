cd66061b5f6d21ee51aef85c8b3c6c4bfed31113
Romain niam0r <niamor@mm.st>
2018-04-04 16:53:16 +0100

    implemented how to handle unrelated actions

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,12 @@
 export default function({ dispatch }) {
   return next => action => {
-    console.log(action);
+    // if the action doesn't have a payload or
+    // if payload doesn't have a .them (not a promise )
+    // send it on
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
 
-    next(action);
+    con
   };
 }
